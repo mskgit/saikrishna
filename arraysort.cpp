f0f8288e689b93aad9dd17ad72256e03d18d11f4
mskgit <54887213+mskgit@users.noreply.github.com>
2019-09-04 14:49:16 +0530

    Create arraysort.cpp

---
+++ arraysort.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main()
+{
+  int arr[5]={5,8,7,9,6};
+  sort(arr,arr+5);
+  for(int i=0;i<5;i++)
+  {
+    cout<<arr[i];
+  }
+    return 0;
+ }
